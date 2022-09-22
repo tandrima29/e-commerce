39b379e91aa584558029fad6d99f949a762cc29c
narayan25-pixel <55695796+narayan25-pixel@users.noreply.github.com>
2022-09-22 12:47:17 +0530

    Navbar added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "../layout/Footer";
-import Header from "../layout/Header";
+import Footer from "../component/Footer";
+import Header from "../component/Header";
 import Home from "./Home";
 
 export default function Dashboard() {
