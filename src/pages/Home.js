552fcbc0db58a7da8434b70d4a19f04750dd1bce
Tandrima <sarkartandrima123@gmail.com>
2022-09-23 22:55:29 +0530

    feature/footer/productcard  updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../component/hero-section/Hero";
 import ProductCard from "../component/product-card/ProductCard";
 
 export default function Home() {
   return (
     <div>
       Home
+      <Hero />
       <ProductCard />
     </div>
   );
