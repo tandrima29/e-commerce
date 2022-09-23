7f936ad113734d2212c507ae1b594fb7f997d348
narayan25-pixel <55695796+narayan25-pixel@users.noreply.github.com>
2022-09-23 14:19:19 +0530

    Product Section added

---
+++ src/component/Categoriescard.js
@@ -0,0 +1,20 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function (props) {
+  return (
+    <div>
+      <Card className="border-0">
+        <Card.Img
+          variant="top"
+          src={props.categoryImage}
+          className="rounded-circle border border-1"
+        />
+        <Card.Body className="text-center">
+          <Card.Text>{props.body}</Card.Text>
+          <Button variant="success">Shop</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
